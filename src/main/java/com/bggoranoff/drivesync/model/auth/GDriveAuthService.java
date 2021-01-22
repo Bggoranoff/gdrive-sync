6252ad22132901f3902b51d171560f1ab9cd347e
Boris Goranoff <boris.goranov.bg@gmail.com>
2021-01-22 17:15:15 +0000

    Feat: drive service authorisation

---
+++ src/main/java/com/bggoranoff/drivesync/model/auth/GDriveAuthService.java
@@ -0,0 +1,55 @@
+package com.bggoranoff.drivesync.model.auth;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class GDriveAuthService {
+    private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+    private Drive driveService = null;
+
+    private Credential retrieveCredentials(final NetHttpTransport HTTP_TRANSPORT, final String CREDENTIALS_PATH, final String TOKENS_PATH) throws IOException {
+        InputStream inputStream = GDriveAuthService.class.getResourceAsStream(CREDENTIALS_PATH);
+        if(inputStream == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(inputStream));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder()
+                .setPort(8888)
+                .build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public void activateDriveService(String applicationName, String credentialsPath, String tokensPath) throws GeneralSecurityException, IOException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        this.driveService = new Drive.Builder(HTTP_TRANSPORT, this.JSON_FACTORY, retrieveCredentials(HTTP_TRANSPORT, credentialsPath, tokensPath))
+                .setApplicationName(applicationName)
+                .build();
+    }
+
+    public Drive getDriveService() {
+        return this.driveService;
+    }
+}
